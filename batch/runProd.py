d7f084ab20cb190ba2a08eb2231516704152dfd9
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2026-09-14 09:28:54 +0000

    Removing extra print statements

--- batch/runProd.py
+++ batch/runProd.py
@@ -154,7 +154,6 @@ if __name__=="__main__":
     else: keep_output = [pos_name if isinstance(pos_name,str) else chain[pos_name] for pos_name in args.store]
     local_opts = defaultdict(str)
     for key,val in args.local:
-        print(key,val)
         local_opts[key if isinstance(key,str) else chain[key]] = val
 
     # list of CMSSW versions for different steps
